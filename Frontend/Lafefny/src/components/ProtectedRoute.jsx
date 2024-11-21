4233aa2c3770b666bcab64d0eb6d416901fb4e9d
Kimo1312 <kareemdarwesh77@gmail.com>
2024-11-21 17:02:47 +0200

    Auth update

--- Frontend/Lafefny/src/components/ProtectedRoute.jsx
+++ Frontend/Lafefny/src/components/ProtectedRoute.jsx
@@ -1,11 +1,28 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
   const userID = localStorage.getItem('userID');
-  
+
+  useEffect(() => {
+    if (!userID) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Access Denied',
+        text: 'You Must Log in First',
+        confirmButtonColor: '#d33',
+        timer: 3000,
+        timerProgressBar: true,
+      }).then(() => {
+        navigate('/');
+      });
+    }
+  }, [userID, navigate]);
+
   if (!userID) {
-    // Redirect to sign-in if no userID is found
-    return <Navigate to="/" replace />;
+    return null;
   }
 
   return children;
