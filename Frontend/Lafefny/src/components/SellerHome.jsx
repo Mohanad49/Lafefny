f06776572b275af7ae881a90fbc42fb5d4a95033
Mohanad <mohanned.3262@gmail.com>
2024-11-23 18:51:57 +0200

    Refactor SellerHome component to organize navigation links into sections

--- Frontend/Lafefny/src/components/SellerHome.jsx
+++ Frontend/Lafefny/src/components/SellerHome.jsx
@@ -13,18 +13,27 @@ const SellerHome = () => {
           <NotificationBell />
         </div>
       </div>
+
       <nav className="navigation">
+        <section>
         <h2>Management</h2>
+        <div className="link-group">
         <Link to="/products">View Products</Link> | {' '}
         <Link to="/add-product">Add Product</Link> | {' '}
         <Link to="/sellerInfo">view info</Link> | {' '}
         <Link to="/addSellerInfo">add info</Link> | {' '}
         <Link to="/editSellerInfo">edit info</Link> | {' '}
         <Link to="/editSellerLogo">edit Logo</Link>
-        
+        </div>
+        </section>
+       
+        <section>
         <h2>Profile Management</h2>
+        <div className="link-group">
         <Link to="/changePassword">Change Password</Link> | {' '}
         <Link to="/delete-account" className="delete-link">Delete Account</Link>
+        </div>
+        </section>
       </nav>
     </div>
   );
